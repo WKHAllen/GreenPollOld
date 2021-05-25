b0d1ca46bb1baaa627c1e0d34b977ce5371c51ae
WKHAllen <wkhallen@gmail.com>
2021-05-25 10:54:28 -0400

    Begin routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { IndexComponent } from './index.component';
+import { NotFoundComponent } from './not-found.component';
+
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
