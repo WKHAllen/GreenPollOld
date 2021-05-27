82171eae19bcaa6a142559c5ae0e902ac5588a19
WKHAllen <wkhallen@gmail.com>
2021-05-26 22:44:04 -0400

    Use withCredentials

--- src/app/login-register/login-register.service.ts
+++ src/app/login-register/login-register.service.ts
@@ -17,6 +17,7 @@ export class LoginRegisterService {
       this.http
         .get<GenericResponse>(APIURL + '/register', {
           params: { username, email, password },
+          withCredentials: true,
         })
         .subscribe((res) => {
           if (res.success) {
@@ -33,6 +34,7 @@ export class LoginRegisterService {
       this.http
         .get<GenericResponse>(APIURL + '/login', {
           params: { email, password },
+          withCredentials: true,
         })
         .subscribe((res) => {
           if (res.success) {
@@ -46,20 +48,26 @@ export class LoginRegisterService {
 
   public async logout(): Promise<void> {
     return new Promise((resolve, reject) => {
-      this.http.get<GenericResponse>(APIURL + '/logout').subscribe((res) => {
-        if (res.success) {
-          resolve();
-        } else {
-          reject(res.error);
-        }
-      });
+      this.http
+        .get<GenericResponse>(APIURL + '/logout', {
+          withCredentials: true,
+        })
+        .subscribe((res) => {
+          if (res.success) {
+            resolve();
+          } else {
+            reject(res.error);
+          }
+        });
     });
   }
 
   public async logoutEverywhere(): Promise<void> {
     return new Promise((resolve, reject) => {
       this.http
-        .get<GenericResponse>(APIURL + '/logout_everywhere')
+        .get<GenericResponse>(APIURL + '/logout_everywhere', {
+          withCredentials: true,
+        })
         .subscribe((res) => {
           if (res.success) {
             resolve();
