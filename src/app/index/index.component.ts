131e923de6f00c2624a5eda2fb62ccd6bc7f8009
WKHAllen <wkhallen@gmail.com>
2021-05-30 12:11:40 -0400

    Show new poll button on index page when logged in

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginRegisterService } from '../login-register/login-register.service';
 
 @Component({
   selector: 'gp-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss'],
 })
-export class IndexComponent {}
+export class IndexComponent implements OnInit {
+  loggedIn = false;
+
+  constructor(private loginRegisterService: LoginRegisterService) {}
+
+  ngOnInit() {
+    this.loggedIn = this.loginRegisterService.loggedIn();
+  }
+}
